a79bf3f5bdd3fbf18df6dc4343be2eeb79e14742
Mohammed Masudur Rahman <masud@outlook.com>
2022-01-09 16:32:20 +0600

    Add files via upload

---
+++ ShortProjects/choose_your_adventure.py
@@ -0,0 +1,32 @@
+from typing import SupportsAbs
+
+
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way do you want to go? ").lower()
+
+if answer == "left":
+    answer = input("You come to a river, you can walk around it or swim across. Type walk to walk around or swim to swim across: ")
+    if answer == "swim":
+        print("You swam across and were eaten by an alligator.")
+    elif answer == "swim":
+        print("You walked for many miles, ran out of water and you lost the game.")
+    else:
+        print('Not a valid option. You lose.')
+elif answer == "right":
+    answer = input("You come to a bridge. It looks wobbly. Do you want to cross it or head back (cross/back)? ")
+    if answer == "back":
+        print("You go back and lose.")
+    elif answer == "cross":
+        answer = input("You cross the bridge and meet a stranger. Do you talk to them? (Yes/ No) ")
+        if answer == "yes":
+            print("You talk to the stranger. They give you gold. You win!")
+        elif answer == "no":
+            print("You ignore the stranger, and they are offended. You lose!")
+    else:
+        print('Not a valid option. You lose.')
+else:
+    print('Not a valid option. You lose.')
+
+print("Thank you for trying, ",name)
